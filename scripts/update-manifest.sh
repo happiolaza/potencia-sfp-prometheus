816677c6fd97fd028030b06a2cf7f4aac0806bf4
happiolaza <happiolaza@users.noreply.github.com>
2026-06-19 16:16:26 -0300

    ci: remove GIT_STRATEGY none, let runner clone natively

--- scripts/update-manifest.sh
+++ scripts/update-manifest.sh
@@ -7,5 +7,4 @@ git config user.name "GitLab CI"
 git config user.email "ci@whitecicd"
 git add deploy/values.yaml
 git commit -m "chore: update image tag to ${CI_COMMIT_SHORT_SHA} [skip ci]"
-git remote set-url origin https://gitlab-ci-token:${CI_JOB_TOKEN}@whitecicd-tt.cuyows.tcloud.ar/operaciones-red-cloud/potencia-sfp-prometheus.git
 git push origin HEAD:main
