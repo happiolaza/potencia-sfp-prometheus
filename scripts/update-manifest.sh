ba1fc876a06dd1828d7de0e9d027d80e76d3c86e
happiolaza <happiolaza@users.noreply.github.com>
2026-06-19 16:25:10 -0300

    ci: skip SSL verification in curl for self-signed cert

--- scripts/update-manifest.sh
+++ scripts/update-manifest.sh
@@ -10,7 +10,7 @@ git config user.email "ci@whitecicd"
 git add deploy/values.yaml
 git commit -m "chore: update image tag to ${CI_COMMIT_SHORT_SHA} [skip ci]"
 
-curl -s --fail -X PUT --header "JOB-TOKEN: ${CI_JOB_TOKEN}" \
+curl -sk --fail -X PUT --header "JOB-TOKEN: ${CI_JOB_TOKEN}" \
   "https://whitecicd-tt.cuyows.tcloud.ar/api/v4/projects/${CI_PROJECT_ID}/repository/files/deploy%2Fvalues.yaml" \
   --data-urlencode "branch=${CI_COMMIT_BRANCH}" \
   --data-urlencode "content=$(cat deploy/values.yaml)" \
