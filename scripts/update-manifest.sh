0144c31f54570d16d417b5074f885a9c03daecc9
happiolaza <happiolaza@users.noreply.github.com>
2026-06-30 15:57:45 -0300

    fix: usar ruta completa de git en update-manifest.sh

--- scripts/update-manifest.sh
+++ scripts/update-manifest.sh
@@ -3,8 +3,8 @@ set -e
 
 sed -i "s|^  tag: .*|  tag: ${CI_COMMIT_SHORT_SHA}|" deploy/values.yaml
 
-git config user.name "GitLab CI"
-git config user.email "ci@whitecicd"
-git add deploy/values.yaml
-git diff --cached --quiet || git commit -m "chore: update image tag to ${CI_COMMIT_SHORT_SHA} [skip ci]"
-git push origin HEAD:"${CI_COMMIT_BRANCH}"
+/usr/bin/git config user.name "GitLab CI"
+/usr/bin/git config user.email "ci@whitecicd"
+/usr/bin/git add deploy/values.yaml
+/usr/bin/git diff --cached --quiet || /usr/bin/git commit -m "chore: update image tag to ${CI_COMMIT_SHORT_SHA} [skip ci]"
+/usr/bin/git push origin HEAD:"${CI_COMMIT_BRANCH}"
